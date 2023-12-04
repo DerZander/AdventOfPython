a9f0430a3f355ed91f2cdc0cfb627c0ac3117b22
Timo Zander <timo.zander@outlook.com>
2023-12-04 21:11:46 +0100

    make it for everybody

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ from AdventOfCoder import AdventOfCoder
 
 if __name__ == "__main__":
     aoc = AdventOfCoder()
-    aoc.get_day(2023, 3)
+    for year in range(2015, 2023):
+        for day in range(1, 26):
+            aoc.get_input_data(year, day)
+    # aoc.get_input_data(2023, 2)
+
