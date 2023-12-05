cf67fca24c0d12a6d72852a9325061586ddd2213
Timo Zander <timo.zander@outlook.com>
2023-12-05 09:25:32 +0100

    solve day 4

--- src/years/year2023/day04/solution.py
+++ src/years/year2023/day04/solution.py
@@ -8,11 +8,11 @@ class Solution(BaseSolution):
         super().__init__()
         self.answer_test = 0
         self.answer_one = 0
-        self.answer_two = 0
-        self.deck = []
+        self.answer_two = 0 # total of scratchcards end up
+        self.deck = {}
 
     def setup(self):
-        self.deck = []
+        self.deck = {}
 
     def get_card(self, line):
         line = line.replace("  ", " ")
@@ -28,22 +28,43 @@ class Solution(BaseSolution):
             "id": int(card_info.replace("  ", " ").split(" ")[1]),
             "winning_numbers": winning_numbers,
             "scratch_numbers": scratch_numbers,
-            "points": 0
+            "points": 0,
+            "winning_numbers_count": 0,
+            "amount": 1
         }
 
         for number in card["scratch_numbers"]:
             if number in card["winning_numbers"]:
+                card["winning_numbers_count"] += 1
                 if card["points"] < 2:
                     card["points"] += 1
                 else:
                     card["points"] = card["points"]*2
-        self.deck.append(card)
+        self.deck[card["id"]] = card
         return card
 
     def solve_test(self):
         for line in self.input_test_data.splitlines():
             card = self.get_card(line)
-            self.answer_test += card["points"]
+
+
+        for index, card in self.deck.items():
+            for amount in range(card["amount"]):
+                for draw_cards in range(card["winning_numbers_count"]):
+                    self.deck[index+draw_cards+1]["amount"] += 1
+
+
+        for index, card in self.deck.items():
+            print("CARD:", card)
+
+        for index, card in self.deck.items():
+            self.answer_test += card["amount"]
+
+
+
+        print(self.deck)
+            # print(card)
+
 
     def solve_one(self):
         for line in self.input_data.splitlines():
@@ -51,9 +72,23 @@ class Solution(BaseSolution):
             self.answer_one += card["points"]
 
     def solve_two(self):
-        self.answer_two = ""
+        for line in self.input_data.splitlines():
+            card = self.get_card(line)
+
+
+        for index, card in self.deck.items():
+            for amount in range(card["amount"]):
+                for draw_cards in range(card["winning_numbers_count"]):
+                    self.deck[index+draw_cards+1]["amount"] += 1
+
+
+        for index, card in self.deck.items():
+            print("CARD:", card)
+
+        for index, card in self.deck.items():
+            self.answer_two += card["amount"]
 
 
 if __name__ == "__main__":
     solution = Solution()
-    solution.solve(0)
+    solution.solve()
