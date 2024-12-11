146284a77c3a9500e02f9eac685346c55f5041a5
n428907 <n428907@scts.santander.de>
2024-12-11 13:25:49 +0100

    solved day 11.1 for year 2024

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 from AdventOfCoder import AdventOfCoder
 
 if __name__ == "__main__":
     aoc = AdventOfCoder(is_work=True)
-    # aoc.get_day(2024, datetime.now().date().day)
-    aoc.get_day(2024, 7)
+    current_day = datetime.now().date().day
+    aoc.get_day(2024, current_day)
+    # aoc.get_day(2024, 11)
